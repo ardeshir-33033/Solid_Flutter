805de51cb0ac2ba731ab05e68204aa4fdbb57c77
ardeshir <ardeshirojan@gmai.com>
2024-01-23 00:35:32 +0330

    Single responsibility

---
+++ lib/single_responsibility/single_responsibilty.dart
@@ -0,0 +1,12 @@
+/// Principal A: Actually the easiest one, a class should have only one responsibility
+// For instance, in a simple app one class should handle the data and another class handle our UI.
+
+
+// Example of adhering to SRP:
+class TaskDataSource {
+  // methods to handle data
+}
+
+class TaskListWidget extends StatelessWidget {
+  // UI code to display a list of tasks
+}
